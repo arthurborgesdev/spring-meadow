b11b7b5c54d00f8bc26a3ea057d0c02efcc1bbdd
arthurmoises <arthurmoises@hotmail.com>
2018-06-14 16:38:15 -0300

    Changed post from Sails server to heroku energizei server

--- led_module.js
+++ led_module.js
@@ -3,8 +3,27 @@ const led = new Gpio(23, {mode: Gpio.OUTPUT});
 
 var ledState = true;
 exports.blink = function() {
+  /*
+  li
+   li
+  (aqui preciso converter os valores de true e
+  false para 1 e 0, a fim de poder passar esses valores
+  para a função led.digitalWrite)
+  */
   if(ledState == true) ledState = 1;
   if(ledState == false) ledState = 0;
+  /*
+  li
+   li
+
+  se ledState == true
+   ledState
+    1
+
+  se ledState == false
+   ledState
+    0
+  */
   //console.log(ledState);
   led.digitalWrite(ledState);
   //console.log(ledState);
@@ -17,6 +36,10 @@ exports.blink = function() {
   }, 500);
 }
 
+//li
+// li
+//
+// (Aqui acende o led e o apaga após meio segundo)
 exports.blinkOnce = function() {
   led.digitalWrite(1);
   setTimeout(function() {
--- weather.js
+++ weather.js
@@ -1,3 +1,6 @@
+//li
+// li
+
 const request = require('request');
 var moment = require('moment');
 
@@ -22,9 +25,13 @@ exports.get = function(err, temperature, humidity) {
   // do range das last Temp e Humid
   if( (temperatureNow*10 > (lastTemp*10 + 2)) || (temperatureNow*10 < (lastTemp*10 - 2)) ||
       (humidityNow*10 > (lastHumid*10 + 5)) || (humidityNow*10 < (lastHumid*10 - 5))  ) {
+      // (Aqui o range é de 0,2 graus para temperatura e de 0,5 para umidade)
+
+
     lastTemp = temperature.toFixed(1);
     lastHumid = humidity.toFixed(1);
-
+    //(Aqui o weather já faz o post com o time_iso e com o device_id e com o
+    // tipo da mensagem)
     var requestForm = {
       time_iso: moment().format(),
       device_id: process.env.RESIN_DEVICE_UUID, // dev_id: process.env.RESIN_DEVICE_UUID,
@@ -32,14 +39,26 @@ exports.get = function(err, temperature, humidity) {
       temp: temperatureNow,
       humid: humidityNow
     };
+    /*
+    ?
+     implementar post sem usar sockets para server Sails
+      usar Https post
+    */
+    request.post("https://energizei-server.herokuapp.com/weather", requestForm, function(err, httpResponse, body) {
+      if (err) console.log(err);
+      console.log(httpResponse);
+      console.log(body);
+    })
 
+
+    /* Função antiga, na época do servidor em Sails
     io.socket.post('/data', requestForm, function serverResponded (body, JWR) {
       // body === JWR.body
       console.log('Sails responded with: ', body);
       console.log('with headers: ', JWR.headers);
       console.log('and with status code: ', JWR.statusCode);
     });
-
+    */
     console.log('temp: ' + temperatureNow + 'C, ' + 'humidity: ' + humidityNow +  '%');
   }
 };
